44f49b6b1d5600276768b7e9a32fff76c9cfc9a7
suchcl <ya5969@163.com>
2021-12-03 17:43:16 +0800

    flow 类型检查

---
+++ flow/src/t1.js
@@ -0,0 +1,14 @@
+// @flow
+
+function fn(x: number, y: number): number {
+    return x + y;
+}
+
+fn(2, 3);
+
+
+function fn2(x: Number, y: Number) {
+    // 实现
+}
+
+fn2(new Number(12), new Number(18));
