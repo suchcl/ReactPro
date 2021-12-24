6d54e55ed78ac585eae064a2eccbbf7c17286570
suchcl <ya5969@163.com>
2021-12-24 10:32:56 +0800

    withRouter

--- react_router530/src/components/Header/Header.jsx
+++ react_router530/src/components/Header/Header.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import "./Header.module.css";
 
-export default class Header extends Component {
+class Header extends Component {
+  forward = () => {
+    this.props.history.goForward();
+  };
+
+  back = () => {
+    this.props.history.goBack();
+  };
+
+  go = () => {
+    this.props.history.go(1);
+  };
+
   render() {
     return (
       <div className="page-header">
         <h2>React Router Demo</h2>
+        <div className="btn-area">
+          <button className="btn" onClick={this.forward}>
+            前进
+          </button>
+          <button className="btn" onClick={this.back}>
+            后退
+          </button>
+          <button className="btn" onClick={this.go}>
+            go
+          </button>
+        </div>
       </div>
     );
   }
 }
+
+export default withRouter(Header);
