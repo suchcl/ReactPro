b404a4a6273e1300de2ff2950b49f6dbf67ff448
suchcl <ya5969@163.com>
2022-01-25 22:18:02 +0800

    hooks 计数器demo

--- react_hooks/src/components/Counter.jsx
+++ react_hooks/src/components/Counter.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-function CountLabel() {
-
+function CountLabel({ count }) {
+    // 子组件用于显示颜色
+    const textColor = count > 10 ? "red" : "orange";
+    return <span style={{ color: textColor }}>{count}</span>
 }
 
 export default function Counter() {
     const [count, setCount] = React.useState(0);
     return <div>
-        <button onClick={() => setCount(count + 1)}>{count}</button>
+        <button onClick={() => setCount(count + 1)}>
+            <CountLabel count={count} />
+        </button>
     </div>;
 }
