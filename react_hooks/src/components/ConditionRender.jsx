b6ef3059f6aa6c35b22f29d3d45834914bce2d3e
suchcl <ya5969@163.com>
2022-02-08 13:47:08 +0800

    函数式组件中条件判断渲染元素

--- react_hooks/src/components/ConditionRender.jsx
+++ react_hooks/src/components/ConditionRender.jsx
@@ -1,10 +1,62 @@
 import React, { useState } from 'react';
 
 export default function ConditionRender() {
-    const userInfo = { name: "Nicholas Zakas", age: 18 };
+    const userInfo = { name: "Nicholas Zakas", age: 18, job: "" };
     const [user, setUser] = useState(userInfo);
+
+    const newsList = [
+        {
+            id: 1,
+            title: "新闻1"
+        },
+        {
+            id: 2,
+            title: "新闻2"
+        },
+        {
+            id: 3,
+            title: "新闻3"
+        },
+        {
+            id: 4,
+            title: "新闻4"
+        },
+        {
+            id: 5,
+            title: "新闻5"
+        },
+    ];
+    const [news, setNews] = useState(newsList);
+
+    const [isShow, setIsShow] = useState(true);
+    const showOrHideText = (event) => {
+        if (isShow) {
+            setIsShow(false);
+            event.target.innerText = "展示";
+        } else {
+            setIsShow(true);
+            event.target.innerText = "隐藏";
+        }
+    }
     return <div>
+        <div style={{ background: "#8c0", padding: '20px' }}>
+            {
+                isShow && (<div>我是判断需要展示和隐藏的文案</div>)
+            }
+            <button onClick={showOrHideText}>隐藏</button>
+        </div>
         <h4>用户名: {user.name}</h4>
         <p>年龄: {user.age}</p>
+        {
+            user.job ? (<p>工作: {user.job}</p>) : (<p>无工作</p>)
+        }
+        <ul className='news'>
+            {
+                // news.map(item => {
+                //     return <li key={item.id}>{item.title}</li>
+                // })
+                news.map(item => <li key={item.id}>{item.title}</li>)
+            }
+        </ul>
     </div>;
 }
